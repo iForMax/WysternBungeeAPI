8f5ad68c563223fac390159657633ba3956bf775
iForMax <feras.gamer.pro@gmail.com>
2025-05-29 22:10:15 +0300

    updates

--- src/main/java/me/firas/corebungeeapi/CoreBungeeAPI.java
+++ src/main/java/me/firas/corebungeeapi/CoreBungeeAPI.java
@@ -1,9 +1,10 @@
 package me.firas.corebungeeapi;
 
 import lombok.Getter;
+import lombok.Setter;
 import me.firas.corebungeeapi.interfaces.Serverable;
 import net.md_5.bungee.api.plugin.Plugin;
-
+@Setter
 public final class CoreBungeeAPI extends Plugin {
 
     @Getter
